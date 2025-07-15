38fa4a94e5023561270302564bf659b92808cc18
alashchev17 <andrew.lashchev15@gmail.com>
2025-07-15 13:13:31 +0200

    feat: functionally improved HeroSection

--- src/design/shared/ProgramCarousel/ProgramCarousel.tsx
+++ src/design/shared/ProgramCarousel/ProgramCarousel.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { FC, useMemo, useState } from 'react'
+import { FC, useMemo, useRef, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
+import { Navigation, Autoplay } from 'swiper/modules'
 import type { Swiper as SwiperType } from 'swiper'
 import { Button } from '#/design/ui'
 import { Typography } from '../Typography'
@@ -20,9 +20,16 @@ interface ProgramCarouselProps {
   type: 'diet' | 'training'
 }
 
+const SLIDER_AUTOPLAY_SPEED = 3_000
+const INITIAL_SLIDE_DELAY = 1_500
+
 export const ProgramCarousel: FC<ProgramCarouselProps> = ({ programs, type }) => {
   const [activeIndex, setActiveIndex] = useState(0)
   const [_swiper, setSwiper] = useState<SwiperType | null>(null)
+
+  const hasStarted = useRef(false)
+  const autoplayIters = useRef(0)
+  const programCarouselRef = useRef<SwiperRef>(null)
 
   const programHref = useMemo(() => {
     return programs[activeIndex].slug?.current ? `/program/${programs[activeIndex].slug.current}` : '#'
@@ -38,73 +45,82 @@ export const ProgramCarousel: FC<ProgramCarouselProps> = ({ programs, type }) =>
 
   if (type === 'diet') {
     return (
-      <div className="relative program-carousel">
-        <Swiper
-          modules={[Navigation]}
-          spaceBetween={24}
-          slidesPerView={1}
-          navigation={{
-            prevEl: '.diet-carousel-prev',
-            nextEl: '.diet-carousel-next',
-          }}
-          loop={programs.length > 1}
-          onSwiper={setSwiper}
-          onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
-          className="rounded-lg h-full"
-          autoplay={{
-            delay: 200,
-            disableOnInteraction: false,
-            pauseOnMouseEnter: false,
-          }}
-          speed={1000}
-        >
-          {programs.map((program) => (
-            <SwiperSlide key={program._id}>
-              <div className="relative h-full flex flex-col justify-between rounded-lg overflow-hidden">
-                {program.imagery?.splash && (
-                  <div className="absolute inset-0">
-                    <Image src={urlFor(program.imagery.splash).url()} alt={program.title || 'Program'} fill className="object-contain" />
+      <>
+        <div className="relative program-carousel">
+          <Swiper
+            modules={[Navigation, Autoplay]}
+            spaceBetween={24}
+            slidesPerView={1}
+            navigation={{
+              prevEl: '.diet-carousel-prev',
+              nextEl: '.diet-carousel-next',
+            }}
+            loop={programs.length > 1}
+            onSwiper={setSwiper}
+            onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+            onAutoplayStart={() => (hasStarted.current = true)}
+            className="rounded-lg h-full"
+            autoplay={{
+              delay: SLIDER_AUTOPLAY_SPEED,
+              disableOnInteraction: true,
+              pauseOnMouseEnter: false,
+            }}
+            speed={1000}
+          >
+            {programs.map((program) => (
+              <SwiperSlide key={program._id}>
+                <div className="relative h-full flex flex-col justify-between rounded-lg overflow-hidden">
+                  {program.imagery?.splash && (
+                    <div className="absolute inset-0">
+                      <Image src={urlFor(program.imagery.splash).url()} alt={program.title || 'Program'} fill className="object-contain" />
+                    </div>
+                  )}
+                  <div className="relative h-full flex flex-col items-center justify-center p-8 z-10">
+                    <Typography
+                      variant="bottoms"
+                      as="span"
+                      className="inline-block px-3 py-1.5 rounded-3xl uppercase bg-dark-gray/70 text-white"
+                    >
+                      {program.title}
+                    </Typography>
                   </div>
-                )}
-                <div className="relative h-full flex flex-col items-center justify-center p-8 z-10">
-                  <Typography
-                    variant="bottoms"
-                    as="span"
-                    className="inline-block px-3 py-1.5 rounded-3xl uppercase bg-dark-gray/70 text-white"
-                  >
-                    {program.title}
-                  </Typography>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        {programs.length > 1 && (
-          <>
-            <Button
-              type="button"
-              variant="icon"
-              className={`diet-carousel-prev bg-transparent border-transparent absolute left-0 top-1/2 -translate-y-1/2 z-10`}
-              icon={<ArrowRight className="rotate-180" />}
-              aria-label="Previous"
-            />
-            <Button
-              type="button"
-              variant="icon"
-              className={`diet-carousel-next bg-transparent border-transparent absolute right-0 top-1/2 -translate-y-1/2 z-10`}
-              icon={<ArrowRight />}
-              aria-label="Next"
-            />
-          </>
-        )}
-      </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {programs.length > 1 && (
+            <>
+              <Button
+                type="button"
+                variant="icon"
+                className={`diet-carousel-prev bg-transparent border-transparent absolute left-0 top-1/2 -translate-y-1/2 z-10`}
+                icon={<ArrowRight className="rotate-180" />}
+                aria-label="Previous"
+              />
+              <Button
+                type="button"
+                variant="icon"
+                className={`diet-carousel-next bg-transparent border-transparent absolute right-0 top-1/2 -translate-y-1/2 z-10`}
+                icon={<ArrowRight />}
+                aria-label="Next"
+              />
+            </>
+          )}
+        </div>
+        <Link href={programHref}>
+          <Button variant="special-light" icon={<ArrowRight />} className="mx-auto block">
+            Обрати дієту
+          </Button>
+        </Link>
+      </>
     )
   }
 
   return (
     <div className="relative program-carousel h-full">
       <Swiper
-        modules={[Navigation]}
+        ref={programCarouselRef}
+        modules={[Navigation, Autoplay]}
         slidesPerView={1}
         navigation={{
           prevEl: '.program-carousel-prev',
@@ -112,9 +128,17 @@ export const ProgramCarousel: FC<ProgramCarouselProps> = ({ programs, type }) =>
         }}
         loop={programs.length > 1}
         onSwiper={setSwiper}
+        onAutoplayStart={() => (hasStarted.current = true)}
         onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
         className="rounded-lg h-full"
-        autoplay
+        autoplay={{
+          delay: !hasStarted.current || autoplayIters.current === 0 ? SLIDER_AUTOPLAY_SPEED + INITIAL_SLIDE_DELAY : SLIDER_AUTOPLAY_SPEED,
+          disableOnInteraction: true,
+          pauseOnMouseEnter: false,
+        }}
+        onAutoplay={() => {
+          autoplayIters.current++
+        }}
         speed={1000}
       >
         <div
