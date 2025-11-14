8c7ee213cdbc16baec285e7466f3fd015513eeab
alashchev17 <andrew.lashchev15@gmail.com>
2025-11-14 23:47:12 +0100

    wip: initial desktop collapsed cards layout

--- src/app/programs/page.tsx
+++ src/app/programs/page.tsx
@@ -1,5 +1,21 @@
 import { PageContainer } from "#/design/shared/PageContainer";
+import { fetchProgramsByType, fetchSettings } from "#/sanity/lib/queries";
+import { ProgramsCarousel } from "./ProgramsCarousel";
 
 export default async function ProgramsPage() {
-  return <PageContainer>ProgramsPage</PageContainer>;
+  // TODO: Get language from context/params
+  const language = "ua";
+
+  const [programs, settings] = await Promise.all([
+    fetchProgramsByType("training", language),
+    fetchSettings(language),
+  ]);
+
+  const ctaLabel = settings?.globalCtas?.learnMore || "Дізнатися більше";
+
+  return (
+    <PageContainer className="bg-black">
+      <ProgramsCarousel programs={programs} ctaLabel={ctaLabel} />
+    </PageContainer>
+  );
 }
